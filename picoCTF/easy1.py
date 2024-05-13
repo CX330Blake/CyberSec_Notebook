9c71975cbcfe609194c22954d1a614bd11957dc8
DESKTOP-RIVTRKI\user <timchen975@gmail.com>
2024-05-13 14:21:30 +0800

    add easy1.py

---
+++ picoCTF/easy1.py
@@ -0,0 +1,47 @@
+"""
+    A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 
+   +----------------------------------------------------
+A | A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+B | B C D E F G H I J K L M N O P Q R S T U V W X Y Z A
+C | C D E F G H I J K L M N O P Q R S T U V W X Y Z A B
+D | D E F G H I J K L M N O P Q R S T U V W X Y Z A B C
+E | E F G H I J K L M N O P Q R S T U V W X Y Z A B C D
+F | F G H I J K L M N O P Q R S T U V W X Y Z A B C D E
+G | G H I J K L M N O P Q R S T U V W X Y Z A B C D E F
+H | H I J K L M N O P Q R S T U V W X Y Z A B C D E F G
+I | I J K L M N O P Q R S T U V W X Y Z A B C D E F G H
+J | J K L M N O P Q R S T U V W X Y Z A B C D E F G H I
+K | K L M N O P Q R S T U V W X Y Z A B C D E F G H I J
+L | L M N O P Q R S T U V W X Y Z A B C D E F G H I J K
+M | M N O P Q R S T U V W X Y Z A B C D E F G H I J K L
+N | N O P Q R S T U V W X Y Z A B C D E F G H I J K L M
+O | O P Q R S T U V W X Y Z A B C D E F G H I J K L M N
+P | P Q R S T U V W X Y Z A B C D E F G H I J K L M N O
+Q | Q R S T U V W X Y Z A B C D E F G H I J K L M N O P
+R | R S T U V W X Y Z A B C D E F G H I J K L M N O P Q
+S | S T U V W X Y Z A B C D E F G H I J K L M N O P Q R
+T | T U V W X Y Z A B C D E F G H I J K L M N O P Q R S
+U | U V W X Y Z A B C D E F G H I J K L M N O P Q R S T
+V | V W X Y Z A B C D E F G H I J K L M N O P Q R S T U
+W | W X Y Z A B C D E F G H I J K L M N O P Q R S T U V
+X | X Y Z A B C D E F G H I J K L M N O P Q R S T U V W
+Y | Y Z A B C D E F G H I J K L M N O P Q R S T U V W X
+Z | Z A B C D E F G H I J K L M N O P Q R S T U V W X Y
+
+C: UFJKXQZQUNB 
+K: SOLVECRYPTO
+"""
+
+cipher = "UFJKXQZQUNB"
+key = "SOLVECRYPTO"
+
+pt = ""
+
+for i in range(len(cipher)):
+    shift = ord(key[i]) - 65  # 获取密钥字母的偏移量
+    c = ord(cipher[i])  # 获取密文中当前位置的字母
+
+    c = (c - shift - 65) % 26 + 65  # 应用偏移量进行解密
+    pt += chr(c)  # 将解密后的字母添加到明文中
+
+print(f"Message: {pt}")
